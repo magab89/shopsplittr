6936c1ed3a144838745f1615aa1a069e5822e69a
Magab89 <magab89@gmail.com>
2022-02-12 22:45:00 +0100

    wip table

--- src/components/kifli/index.tsx
+++ src/components/kifli/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { copyPaste } from './test-kifl-order'
 import { processKifli } from './processor'
 import './kifli.css'
+import { useTable } from 'react-table'
 
 export interface Product {
   name: string
@@ -13,22 +14,108 @@ interface OrderProps {
 
 }
 
-function list (items: any) {
+function list (items: Product[]) {
   return (
     <ul>{
-      items.map((item: any) => <li key={item.name}>{`${item.name}, ${item.price}, ${item.amount}`}</li>)
+      items.map((item: Product) => <li key={item.name}>{`${item.name}, ${item.price}, ${item.amount}`}</li>)
     }</ul>
+  )
+}
+
+interface TableeProps {
+  products: Product[]
+}
+
+function Tableee (props: TableeProps) {
+  const data = React.useMemo(
+    () => [...props.products], []
+  )
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Name',
+        accessor: 'name', // accessor is the "key" in the data
+      },
+      {
+        Header: 'Amount',
+        accessor: 'amount',
+      },
+      {
+        Header: 'Price',
+        accessor: 'price',
+      },
+    ],
+    []
+  )
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({ columns, data })
+
+  return (
+    <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
+      <thead>
+      {headerGroups.map(headerGroup => (
+        <tr {...headerGroup.getHeaderGroupProps()}>
+          {headerGroup.headers.map(column => (
+            <th
+              {...column.getHeaderProps()}
+              style={{
+                borderBottom: 'solid 3px red',
+                background: 'aliceblue',
+                color: 'black',
+                fontWeight: 'bold',
+              }}
+            >
+              {column.render('Header')}
+            </th>
+          ))}
+        </tr>
+      ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+      {rows.map(row => {
+        prepareRow(row)
+        return (
+          <tr {...row.getRowProps()}>
+            {row.cells.map(cell => {
+              return (
+                <td
+                  {...cell.getCellProps()}
+                  style={{
+                    padding: '10px',
+                    border: 'solid 1px gray',
+                    background: 'papayawhip',
+                  }}
+                >
+                  {cell.render('Cell')}
+                </td>
+              )
+            })}
+          </tr>
+        )
+      })}
+      </tbody>
+    </table>
   )
 }
 
 export default function Kifli(props: OrderProps) {
   const processed = processKifli(copyPaste)
+
   return ( <>
       <div className='kifli'>
         {list(processed)}
       </div>
       <div>
-        IDE jön majd minden
+        <Tableee
+          products={processed}
+        />
       </div>
   </>
 
